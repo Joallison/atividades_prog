a728ca3379a0c2c02e106d4b9e5ab59b7e366a34
Joallison Avelino <joallison_jose@hotmail.com>
2017-07-10 22:04:05 -0300

    Implementando socket com multithread.

--- atv_multithread/21_multithread.py
+++ atv_multithread/21_multithread.py
@@ -1,2 +1,8 @@
 import socket
 import threading
+
+class ThreadServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock.setsock.socket
